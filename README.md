a2ee256b86f9aa9dfbfb23f43c6e2e8b111851b9
asy3887 <asy3887@northwesternmutual.com>
2025-07-25 14:26:37 -0700

    added wager model and updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # statted-up
-My first legitimate attempt at a coding project. It a sports betting progress tracker. 
+
+My first legitimate attempt at a coding project. It a sports betting progress tracker.
+
+I will need to create a script that runs the front end and back end concurrently when its time to run the full application.
--- server/tracker/src/main/java/com/stattedup/tracker/models/Wager.java
+++ server/tracker/src/main/java/com/stattedup/tracker/models/Wager.java
@@ -12,4 +12,62 @@ public class Wager {
     private WagerResult wagerResult;
     private SportsBook sportsBook;
     private String customSportsBook;
+    private String notes;
+
+    public UUID getWagerId() {
+        return wagerId;
+    }
+
+    public void setWagerId(UUID wagerId) {
+        this.wagerId = wagerId;
+    }
+
+    public List<Bet> getBets() {
+        return bets;
+    }
+
+    public void setBets(List<Bet> bets) {
+        this.bets = bets;
+    }
+
+    public LocalDateTime getDatePlaced() {
+        return datePlaced;
+    }
+
+    public void setDatePlaced(LocalDateTime datePlaced) {
+        this.datePlaced = datePlaced;
+    }
+
+    public WagerResult getWagerResult() {
+        return wagerResult;
+    }
+
+    public void setWagerResult(WagerResult wagerResult) {
+        this.wagerResult = wagerResult;
+    }
+
+    public SportsBook getSportsBook() {
+        return sportsBook;
+    }
+
+    public void setSportsBook(SportsBook sportsBook) {
+        this.sportsBook = sportsBook;
+    }
+
+    public String getCustomSportsBook() {
+        return customSportsBook;
+    }
+
+    public void setCustomSportsBook(String customSportsBook) {
+        this.customSportsBook = customSportsBook;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
 }
